476f5c5b01a142e05e0d2722b8f8dcd1c5577b6f
MedvedKopatych <caligula.sab@mail.ru>
2022-04-08 21:29:04 +0300

    Add Heroku settings to production

--- fieldmod/settings/prod.py
+++ fieldmod/settings/prod.py
@@ -9,5 +9,34 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'field-modernization-WOT.herokuapp.com',
+    'field-modernization-wot.herokuapp.com',
     ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': 'DEBUG',
+        'class': 'logging.StreamHandler',
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            },
+    }
+}
+#  Heroku settings
+#django_on_heroku.settings(locals(), staticfiles=False)
+#del DATABASES['default']['OPTIONS']['sslmode']
